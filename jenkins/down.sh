fe179d28fd8b0019fefc41061a5601bf835dd5f2
Rory Houlihan <rhoulihan@purestorage.com>
2023-10-10 14:15:16 +0200

    remove ingress before removig namespace

--- jenkins/down.sh
+++ jenkins/down.sh
@@ -3,5 +3,5 @@ kubectl delete -f deployment.yaml
 kubectl delete -f volume.yaml
 kubectl delete -f service.yaml
 kubectl delete -f serviceAccount.yaml
-kubectl delete -f namespace.yaml
 kubectl delete -f ingress.yaml
+kubectl delete -f namespace.yaml
